31d1fb9cea3ffb2d88c42b6ba1e0f2abc65f28e6
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-16 19:29:47 +0530

    create div using JS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 // testing JS wiring
 console.log('wired');
+let container = document.querySelector('.container');
+
+const gridGenerator = () => {
+        for(let i=0; i<17; i++){
+                // let myDiv = document.createElement('div');
+                // container.appendChild(myDiv);
+        }
+        
+}
+gridGenerator();
