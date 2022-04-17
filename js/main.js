53445ae8e6cd5d6de709d019a059f834a3aa3ae6
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-17 14:06:18 +0530

    style done for grid

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ console.log('wired');
 let container = document.querySelector('.container');
 
 const gridGenerator = () => {
+
         for(let i=0; i<17; i++){
-                // let myDiv = document.createElement('div');
-                // container.appendChild(myDiv);
+                let myDiv = document.createElement('div');
+                myDiv.setAttribute('class', 'gridDiv')
+                container.appendChild(myDiv);
         }
         
 }
 gridGenerator();
+
