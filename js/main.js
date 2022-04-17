dc382b837ec3cfe3fd9c7a2046f9ff4221aff10c
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-17 14:51:37 +0530

    grid successfully created

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 // testing JS wiring
-console.log('wired');
-let container = document.querySelector('.container');
+// console.log('wired');
 
 const gridGenerator = () => {
+        for (let j = 0; j < 17; j++) {
+                let parent = document.querySelector('.gridContainer');
+                let newDiv = document.createElement('div');
+                newDiv.setAttribute('class', 'container');
+                parent.appendChild(newDiv);
 
-        for(let i=0; i<17; i++){
-                let myDiv = document.createElement('div');
-                myDiv.setAttribute('class', 'gridDiv')
-                container.appendChild(myDiv);
+                let containerAll = document.querySelectorAll('.container');
+                // console.log(containerAll);
+                let container = containerAll[j];
+                // console.log(container);
+                for (let i = 0; i < 17; i++) {
+
+
+                        let myDiv = document.createElement('div');
+                        myDiv.setAttribute('class', 'gridDiv')
+                        container.appendChild(myDiv);
+                }
         }
-        
+
+
 }
 gridGenerator();
 
